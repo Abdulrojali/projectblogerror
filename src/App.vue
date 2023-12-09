958d1788bcbfbcb54fac5dfcb64d53d99bfaa8b0
abdulrojali <rojaliabdul4@gmail.com>
2023-12-09 07:39:30 -0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import {ref} from 'vue'
+export default{
+  setup(){
+    const datas=ref([])
+    const getData=async function(){
+      const datafecth= await fetch('http://localhost:3000/posts')
+      const response=datafecth.response()
+      const convert=response.json()
+      console.log(convert)
+    }
+    return{datas,getData}
+  },
+  mounted(){
+    this.getData()
+  }
+}
+</script>
+
+<template>
+
+</template>
